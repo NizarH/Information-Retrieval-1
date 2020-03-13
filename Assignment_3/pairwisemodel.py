91976f77ecd370d62043797230f80e93c4b9e180
NizarH <nizarhirzalla@hotmail.com>
2020-03-13 22:46:48 +0100

    pairwise + rank + evaluation

---
+++ Assignment_3/pairwisemodel.py
@@ -0,0 +1,114 @@
+import torch.nn as nn
+import ranking as rnk
+import dataset
+import evaluate as eval
+import numpy as np
+import torch
+from tqdm import tqdm
+
+
+
+class pairWiseModel(nn.Module):
+    def __init__(self, num_features, scoring_network_layers, dropout=0.3, sigma=1, pairs=2000):
+        super().__init__()
+        self.scoringModules = nn.ModuleList()
+        input_size = num_features
+        for layer_size in scoring_network_layers:
+            self.scoringModules.append(nn.Sequential(nn.Linear(input_size, layer_size), nn.Dropout(dropout), nn.ReLU()))
+            input_size = layer_size
+        self.name = "Pairwise LTR model"
+        self.scoringModules.append(nn.Linear(layer_size, 1))
+        self.scoringModules.to("cpu")
+        self.loss_fn = torch.nn.BCELoss()
+        self.sigma = sigma
+        self.pairs = pairs
+
+    def score(self, docs):
+        scores = torch.Tensor(docs.feature_matrix)
+        scores = scores.to("cpu")
+        for layer in self.scoringModules:
+            scores = layer(scores)
+        self.scores = scores
+        return scores.detach().numpy().squeeze()
+
+    def rank(self, scores, docs):
+        self.all_rankings, self.all_inverted_rankings = rnk.data_split_rank_and_invert(scores, docs)
+        return self.all_rankings, self.all_inverted_rankings
+
+
+    def calc_ranknet(self, s_i, s_j):
+        #computing cross entropy between desired probability and predicted probablity
+        return 1 / (1 + torch.exp(-self.sigma * (s_i - s_j)))
+
+    def loss_function(self, target):
+        #initiliaze loss and target
+        target = target.squeeze()
+        loss = torch.zeros(1)
+
+        ##get pairs of docs for query
+        for pair in range(self.pairs):
+            #pick two docs at random
+            i, j = np.random.choice(self.scores.shape[0], 2)
+            #scores for document i and j
+            s_i = self.scores[i]
+            s_j = self.scores[j]
+            #Sij is a element of {-1, 0, 1}, with -1 indicating that j is the more relevant document, 1 indicating i
+            # is more relevant and 0 indicating equal relevancy.
+            if target[i] > target[j]:
+                S = 1
+            elif target[i] == target[j]:
+                S = 0
+            else:
+                S = -1
+
+            S = torch.Tensor([S])
+            P = self.calc_ranknet(s_i, s_j)
+            loss += self.loss_fn(P, S)
+
+        return loss
+
+
+def trainModel(model, data, epochs, optimizer):
+    print("======================== " + model.name + "========================")
+
+    labels = torch.Tensor(data.train.label_vector).to("cpu").unsqueeze(1)
+    labels_val = torch.Tensor(data.validation.label_vector).to("cpu").unsqueeze(1)
+
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        optimizer.zero_grad()
+        all_scores = model.score(data.train)
+        loss = model.loss_function(labels)
+        loss.backward()
+        optimizer.step()
+        if epoch%10 == 0:
+            print("validation ndcg at epoch " + str(epoch))
+            model.eval()
+            validation_scores = model.score(data.validation)
+            results = eval.evaluate(data.validation, validation_scores, print_results=False)
+            print(results["ndcg"])
+
+
+
+
+data = dataset.get_dataset().get_data_folds()[0]
+data.read_data()
+epochs = 100
+model = pairWiseModel(data.num_features, [10,10,10])
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
+
+print('Number of features: %d' % data.num_features)
+print('Number of queries in training set: %d' % data.train.num_queries())
+print('Number of documents in training set: %d' % data.train.num_docs())
+print('Number of queries in validation set: %d' % data.validation.num_queries())
+print('Number of documents in validation set: %d' % data.validation.num_docs())
+print('Number of queries in test set: %d' % data.test.num_queries())
+print('Number of documents in test set: %d' % data.test.num_docs())
+
+# initialize a random model
+trainModel(model, data, epochs, optimizer)
+
+test_scores = model.score(data.test)
+print('------')
+print('Evaluation on entire test partition.')
+results = eval.evaluate(data.test, test_scores, print_results=True)
